95174603884721a7b9eef0590659700cd98ebe1f
Andre Taulien <a.taulien@live.de>
2019-04-08 18:39:54 +0200

    Feature: Background image for Font-viewer

--- src/main_FontViewer.cpp
+++ src/main_FontViewer.cpp
@@ -1,17 +1,20 @@
 #include "BsFPSCamera.h"
 #include "REGothEngine.hpp"
 #include <BsZenLib/ImportFont.hpp>
+#include <BsZenLib/ImportTexture.hpp>
 #include <Components/BsCCamera.h>
 #include <GUI/BsCGUIWidget.h>
+#include <Image/BsSpriteTexture.h>
 #include <GUI/BsGUILabel.h>
 #include <GUI/BsGUIPanel.h>
 #include <GUI/BsGUISkin.h>
+#include <GUI/BsGUITexture.h>
 #include <Resources/BsBuiltinResources.h>
 #include <Scene/BsSceneObject.h>
 #include <Text/BsFont.h>
 #include <original-content/VirtualFileSystem.hpp>
 
-class REGothWorldViewer : public REGoth::REGothEngine
+class REGothFontViewer : public REGoth::REGothEngine
 {
 public:
   void setupMainCamera() override
@@ -41,6 +44,19 @@ public:
       exit(-1);
     }
 
+    const char* BACKGROUND = "STARTSCREEN.TGA";
+
+    bs::HTexture backgroundTexture =
+        BsZenLib::ImportAndCacheTexture(BACKGROUND, REGoth::gVirtualFileSystem().getFileIndex());
+
+    if (!backgroundTexture)
+    {
+      bs::gDebug().logError("Failed to load BackgroundTexture: " + bs::String(BACKGROUND));
+      exit(-1);
+    }
+
+    bs::HSpriteTexture backgroundSprite = bs::SpriteTexture::create(backgroundTexture);
+
     // Add GUI
     bs::HSceneObject guiSO = bs::SceneObject::create("GUI");
     bs::HGUIWidget gui = guiSO->addComponent<bs::CGUIWidget>(mMainCamera);
@@ -57,17 +73,20 @@ public:
     gui->setSkin(skin);
 
     bs::GUIPanel* mainPanel = gui->getPanel();
+    bs::GUIPanel* backgroundPanel = mainPanel->addNewElement<bs::GUIPanel>(1);
+
+    bs::GUITexture* background = backgroundPanel->addNewElement<bs::GUITexture>(backgroundSprite);
+    background->setPosition(0,0);
+    // background->setSize(backgroundPanel->getScreenBounds().x, backgroundPanel->getScreenBounds().y);
 
     const char* TEXT =
         "This is some bullshit text for testing. Does this thing handle newlines?\n"
         "I guess we'll see. What about umlauts like ä, ö, ü and ß? UPPERCASE ÄÖÜ?\n"
         "Do special characters work? Something like %, #, / or _?";
 
-    bs::GUILabel* labelDefaultFont =
-        mainPanel->addNewElement<bs::GUILabel>(bs::HString(TEXT));
+    bs::GUILabel* labelDefaultFont = mainPanel->addNewElement<bs::GUILabel>(bs::HString(TEXT));
 
-    bs::GUILabel* labelGothicFont =
-        mainPanel->addNewElement<bs::GUILabel>(bs::HString(TEXT));
+    bs::GUILabel* labelGothicFont = mainPanel->addNewElement<bs::GUILabel>(bs::HString(TEXT));
     labelGothicFont->setStyle("GothicLabelStyle");
 
     labelDefaultFont->setPosition(10, 20);
@@ -80,7 +99,7 @@ protected:
 
 int main(int argc, char** argv)
 {
-  REGothWorldViewer regoth;
+  REGothFontViewer regoth;
 
   return REGoth::main(regoth, argc, argv);
 }
