0ecbb99d990e9ba18d29fe23321db1e6fcf02393
Andre Taulien <a.taulien@live.de>
2019-04-11 21:47:02 +0200

    Extend script tester

--- src/main_ScriptTest.cpp
+++ src/main_ScriptTest.cpp
@@ -1,10 +1,10 @@
 #include "REGothEngine.hpp"
-#include <scripting/ScriptSymbolStorage.hpp>
-#include <scripting/ScriptVMInterface.hpp>
-#include <daedalus/DATFile.h>
 #include <Components/BsCCamera.h>
 #include <Scene/BsSceneObject.h>
 #include <components/Item.hpp>
+#include <daedalus/DATFile.h>
+#include <scripting/ScriptSymbolStorage.hpp>
+#include <scripting/ScriptVMInterface.hpp>
 
 class REGothScriptTester : public REGoth::REGothEngine
 {
@@ -16,16 +16,23 @@ public:
 
   void setupScene() override
   {
+    using namespace REGoth;
+    using ScriptObject       = Scripting::ScriptObject;
+    using ScriptObjectHandle = Scripting::ScriptObjectHandle;
 
     Daedalus::DATFile dat("/home/andre/games/Gothic II/_work/Data/Scripts/_compiled/GOTHIC.DAT");
     REGoth::Scripting::loadGothicDAT(dat);
 
-    REGoth::gGameScript().instanciateClass("C_ITEM", "ITFO_APPLE");
+    ScriptObjectHandle appleHandle = gGameScript().instanciateClass("C_ITEM", "ITFO_APPLE");
 
-    REGoth::gGameScript().script_PrintPlus("Hello World!");
-    REGoth::gGameScript().script_PrintPlus("This is printed by Daedalus!");
-    REGoth::gGameScript().script_PrintPlus("It also appends a random number to the end, look: ");
-    REGoth::gGameScript().script_PrintPlus("Pretty cool, eh?");
+    ScriptObject& apple = REGoth::gGameScript().scriptObjects().get(appleHandle);
+
+    bs::gDebug().logDebug("Apple's visual: " + apple.stringValue("VISUAL"));
+
+    gGameScript().script_PrintPlus("Hello World!");
+    gGameScript().script_PrintPlus("This is printed by Daedalus!");
+    gGameScript().script_PrintPlus("It also appends a random number to the end, look: ");
+    gGameScript().script_PrintPlus("Pretty cool, eh?");
 
     bs::HSceneObject item = bs::SceneObject::create("Apple");
     item->addComponent<REGoth::Item>("ITFO_APPLE");
