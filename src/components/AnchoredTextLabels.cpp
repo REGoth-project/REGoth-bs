2a3cd51417bba8c7e9f0a3dba3b0d206c268ba6c
Markus Worchel <mworchel@hotmail.de>
2019-04-22 09:59:07 +0200

    Formatting

--- src/components/AnchoredTextLabels.cpp
+++ src/components/AnchoredTextLabels.cpp
@@ -20,10 +20,9 @@ namespace REGoth
 
     if (mGui.isDestroyed())
     {
-      REGOTH_THROW(
-          InvalidStateException,
-          bs::StringUtil::format("Scene Object {0} does not have a CGUIWidget component!",
-                                 SO()->getName()));
+      REGOTH_THROW(InvalidStateException,
+                   bs::StringUtil::format("Scene Object {0} does not have a CGUIWidget component!",
+                                          SO()->getName()));
     }
   }
 
@@ -48,10 +47,12 @@ namespace REGoth
 
   void AnchoredTextLabels::update()
   {
-    auto camera                          = mGui->getCamera();
+    auto camera = mGui->getCamera();
+
     const bs::Transform& cameraTransform = camera->getTransform();
     const bs::Vector3& cameraPosition    = cameraTransform.getPosition();
     const bs::Vector3& cameraForward     = cameraTransform.getForward();
+
     for (const auto& anchoredLabel : mLabels)
     {
       // Only show labels with an anchor point in front of the camera
@@ -68,4 +69,4 @@ namespace REGoth
     }
   }
 
-}  // namespace bs
+}  // namespace REGoth
