55427c7fbdff92fd4f03db3df3bbd833b24b5d5a
charlydelta <git@charlydelta.de>
2019-07-31 21:31:36 +0200

    Refactoring: clang-format

--- src/log/logging.hpp
+++ src/log/logging.hpp
@@ -7,14 +7,13 @@
  * BS_LOG will output the location of the log-call by default,
  * which includes the rather lengthy function signature.
  */
-#define REGOTH_LOG(verbosity, category, message, ...)                                   \
-  do                                                                                    \
-  {                                                                                     \
-    using namespace ::bs;                                                               \
-    if (static_cast<INT32>(LogVerbosity::verbosity)                                     \
-        <= static_cast<INT32>(BS_LOG_VERBOSITY))                                        \
-    {                                                                                   \
-      gDebug().log(StringUtil::format(message, ##__VA_ARGS__), LogVerbosity::verbosity, \
-                   LogCategory##category::_id);                                         \
-    }                                                                                   \
+#define REGOTH_LOG(verbosity, category, message, ...)                                        \
+  do                                                                                         \
+  {                                                                                          \
+    using namespace ::bs;                                                                    \
+    if (static_cast<INT32>(LogVerbosity::verbosity) <= static_cast<INT32>(BS_LOG_VERBOSITY)) \
+    {                                                                                        \
+      gDebug().log(StringUtil::format(message, ##__VA_ARGS__), LogVerbosity::verbosity,      \
+                   LogCategory##category::_id);                                              \
+    }                                                                                        \
   } while (0)
