5f8845915658cbcfb383f5fa3eb3291266ebe50c
KirmesBude <kirmesbude@gmail.com>
2019-08-05 18:45:35 +0200

    Certainly looks a lot cleaner

--- src/components/Console.cpp
+++ src/components/Console.cpp
@@ -24,33 +24,31 @@ namespace REGoth
   void Console::onInitialized()
   {
     auto valueChangedCallback = [&](const bs::String& input) {
-      REGOTH_LOG(Info, Uncategorized, "[Console] Input changed to: {0}!", input);
-      bs::Vector<bs::String> suggestions = onInputChanged(input);
+      REGOTH_LOG(Info, Uncategorized, "[Console] !EVENT! Input changed to: {0}!", input);
+      onInputChanged(input);
     };
     mConsoleUI->mOnInputChanged.connect(valueChangedCallback);
 
     auto confirmCallback = [&]() {
       const bs::String& input = mConsoleUI->getInput();
-      REGOTH_LOG(Info, Uncategorized, "[Console] Command confirmed: {0}!", input);
-      bs::Vector<bs::String> outputs = onCommandConfirmed(input);
+      REGOTH_LOG(Info, Uncategorized, "[Console] !EVENT! Command confirmed: {0}!", input);
+      onCommandConfirmed(input);
       mConsoleUI->clearInput();
     };
     mConsoleUI->mOnConfirm.connect(confirmCallback);
   }
 
-  bs::Vector<bs::String> Console::onInputChanged(const bs::String& input)
+  void Console::onInputChanged(const bs::String& input)
   {
-    bs::Vector<bs::String> outputs;
-    return outputs;
+    // TODO + rename
   }
 
-  bs::Vector<bs::String> Console::onCommandConfirmed(bs::String input)
+  void Console::onCommandConfirmed(const bs::String& input)
   {
-    bs::Vector<bs::String> outputs;
-    return outputs;
+    // TODO + rename
   }
 
-  bs::Vector<bs::String> Console::command_List(bs::Vector<bs::String> args)
+  void Console::command_List(bs::Vector<bs::String> args)
   {
     REGOTH_LOG(Info, Uncategorized, "[Console] Command 'list' executed!");
     bs::Vector<bs::String> outputs;
@@ -62,11 +60,9 @@ namespace REGoth
 
       outputs.push_back(command);
     }
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_Help(bs::Vector<bs::String> args)
+  void Console::command_Help(bs::Vector<bs::String> args)
   {
     REGOTH_LOG(Info, Uncategorized, "[Console] Command 'help' executed!");
     bs::Vector<bs::String> outputs;
@@ -84,189 +80,147 @@ namespace REGoth
       bs::String help = it->second.help;
       outputs.push_back(help);
     }
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_CheatFull(bs::Vector<bs::String> args)
+  void Console::command_CheatFull(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'cheat full' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_CheatGod(bs::Vector<bs::String> args)
+  void Console::command_CheatGod(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'cheat god' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_Insert(bs::Vector<bs::String> args)
+  void Console::command_Insert(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'insert' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_Spawnmass(bs::Vector<bs::String> args)
+  void Console::command_Spawnmass(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'spawnmass' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_Kill(bs::Vector<bs::String> args)
+  void Console::command_Kill(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'kill' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_EditAbilities(bs::Vector<bs::String> args)
+  void Console::command_EditAbilities(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'edit abilities' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_EditFocus(bs::Vector<bs::String> args)
+  void Console::command_EditFocus(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'edit focus' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_SetTime(bs::Vector<bs::String> args)
+  void Console::command_SetTime(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'set time' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_GotoWaypoint(bs::Vector<bs::String> args)
+  void Console::command_GotoWaypoint(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'goto waypoint' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_GotoCamera(bs::Vector<bs::String> args)
+  void Console::command_GotoCamera(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'goto camera' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_GotoPos(bs::Vector<bs::String> args)
+  void Console::command_GotoPos(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'goto pos' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_AIGoto(bs::Vector<bs::String> args)
+  void Console::command_AIGoto(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'aigoto' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_SetClippingfactor(bs::Vector<bs::String> args)
+  void Console::command_SetClippingfactor(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized,
                "[Console] Command 'set clippingfactor' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_ZFogZone(bs::Vector<bs::String> args)
+  void Console::command_ZFogZone(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'zfogzone' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_ToggleConsole(bs::Vector<bs::String> args)
+  void Console::command_ToggleConsole(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'toggle console' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_ToggleFrame(bs::Vector<bs::String> args)
+  void Console::command_ToggleFrame(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'toggle frame' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_ToggleWaynet(bs::Vector<bs::String> args)
+  void Console::command_ToggleWaynet(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'toggle waynet' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_Firstperson(bs::Vector<bs::String> args)
+  void Console::command_Firstperson(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'firstperson' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_HeroExport(bs::Vector<bs::String> args)
+  void Console::command_HeroExport(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'hero export' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
-  bs::Vector<bs::String> Console::command_HeroImport(bs::Vector<bs::String> args)
+  void Console::command_HeroImport(bs::Vector<bs::String> args)
   {
     // TODO: implement
     REGOTH_LOG(Warning, Uncategorized, "[Console] Command 'hero import' is not implemented yet!");
     bs::Vector<bs::String> outputs;
-
-    return outputs;
   }
 
   void Console::registerCommand(const bs::String& name, Command command)
--- src/components/Console.hpp
+++ src/components/Console.hpp
@@ -28,8 +28,6 @@ namespace REGoth
      * Keeps track of where we are in the current command to offer 'content-aware' autocomplete
      * suggestions.
      *
-     * @return A vector of auto-complete suggestions to be displayed by the ConsoleUI component.
-     *
      * @note   To be used in the callback of the inputChanged event of the inputBox of the ConsoleUI
      *         component.
      *
@@ -37,12 +35,10 @@ namespace REGoth
      *           Current input - usually provided by the inputChanged event of the inputBox of the
      *           ConsoleUI component.
      */
-    bs::Vector<bs::String> onInputChanged(const bs::String& input);
+    void onInputChanged(const bs::String& input);
 
     /**
      * Identifies the correct command based on the input string and executes its callback.
-     *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @note   To be used in the callback of the onInputConfirmed event of the inputBox of the
      *         ConsoleUI component.
@@ -50,7 +46,7 @@ namespace REGoth
      * @param  input
      *           Current input - Needs to be provided manually by the ConsoleUI component.
      */
-    bs::Vector<bs::String> onCommandConfirmed(bs::String input);
+    void onCommandConfirmed(const bs::String& input);
 
   protected:
     void onInitialized() override;
@@ -59,228 +55,206 @@ namespace REGoth
     /**
      * Lists all commands that are registered to this Console.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_List(bs::Vector<bs::String> args);
+    void command_List(bs::Vector<bs::String> args);
 
     /**
      * Prints a 'helpful' help message (aka a short description) of the given command.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Name of the command.
      */
-    bs::Vector<bs::String> command_Help(bs::Vector<bs::String> args);
+    void command_Help(bs::Vector<bs::String> args);
 
     /**
      * Restores health and mana of the currently controlled(?) character.
      * TODO: currently controlled or PC_HERO only?
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_CheatFull(bs::Vector<bs::String> args);
+    void command_CheatFull(bs::Vector<bs::String> args);
 
     /**
      * Turns on god mode for the currently controlled(?) character.
      * TODO: currently controlled or PC_HERO only?
      * TODO: Is god mode only invulnerability?
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_CheatGod(bs::Vector<bs::String> args);
+    void command_CheatGod(bs::Vector<bs::String> args);
 
     /**
      * Insert the given Item/NPC infront of the currently controlled character.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Name of an item or NPC.
      * TODO: Anything else you can insert with this?
      */
-    bs::Vector<bs::String> command_Insert(bs::Vector<bs::String> args);
+    void command_Insert(bs::Vector<bs::String> args);
 
     /**
      * Spawns (???) around(?) the currently controlled character-
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Amount of entities to spawn.
      */
-    bs::Vector<bs::String> command_Spawnmass(bs::Vector<bs::String> args);
+    void command_Spawnmass(bs::Vector<bs::String> args);
 
     /**
      * Kills target currently in focus.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_Kill(bs::Vector<bs::String> args);
+    void command_Kill(bs::Vector<bs::String> args);
 
     /**
      * Allows editing various attributes and abilities of the currently controlled character.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_EditAbilities(bs::Vector<bs::String> args);
+    void command_EditAbilities(bs::Vector<bs::String> args);
 
     /**
      * Allows editing various attributes and abilities of the character currently in focus.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_EditFocus(bs::Vector<bs::String> args);
+    void command_EditFocus(bs::Vector<bs::String> args);
 
     /**
      * Sets the current time.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Two literal arguments in the form of [hh] [mm].
      */
-    bs::Vector<bs::String> command_SetTime(bs::Vector<bs::String> args);
+    void command_SetTime(bs::Vector<bs::String> args);
 
     /**
      * Teleport the currently controlled character to the given waypoint.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Waypoint to teleport to.
      */
-    bs::Vector<bs::String> command_GotoWaypoint(bs::Vector<bs::String> args);
+    void command_GotoWaypoint(bs::Vector<bs::String> args);
 
     /**
      * Teleports PC_HERO(?) to the free floating camera.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_GotoCamera(bs::Vector<bs::String> args);
+    void command_GotoCamera(bs::Vector<bs::String> args);
 
     /**
      * Teleport the currently controlled character to the given position in the world.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Three literal arguments in the form of [x] [y] [z] to repesent a position.
      */
-    bs::Vector<bs::String> command_GotoPos(bs::Vector<bs::String> args);
+    void command_GotoPos(bs::Vector<bs::String> args);
 
     /**
      * ???
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Waypoint.
      */
-    bs::Vector<bs::String> command_AIGoto(bs::Vector<bs::String> args);
+    void command_AIGoto(bs::Vector<bs::String> args);
 
     /**
      * ???
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Literal float argument.
      */
-    bs::Vector<bs::String> command_SetClippingfactor(bs::Vector<bs::String> args);
+    void command_SetClippingfactor(bs::Vector<bs::String> args);
 
     /**
      * ???
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_ZFogZone(bs::Vector<bs::String> args);
+    void command_ZFogZone(bs::Vector<bs::String> args);
 
     /**
      * Toggles the visuals of the console.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_ToggleConsole(bs::Vector<bs::String> args);
+    void command_ToggleConsole(bs::Vector<bs::String> args);
 
     /**
      * Toggles the visuals of the frame (whole gameui).
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_ToggleFrame(bs::Vector<bs::String> args);
+    void command_ToggleFrame(bs::Vector<bs::String> args);
 
     /**
      * Toggles the visuals of the waynet (debug).
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_ToggleWaynet(bs::Vector<bs::String> args);
+    void command_ToggleWaynet(bs::Vector<bs::String> args);
 
     /**
      * Toggles firstperson mode.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Does not use any arguments.
      */
-    bs::Vector<bs::String> command_Firstperson(bs::Vector<bs::String> args);
+    void command_Firstperson(bs::Vector<bs::String> args);
 
     /**
      * Exports attributes and abilities of currently controlled characters to a file with the given
      * name in the save folder.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Name of the file.
      */
-    bs::Vector<bs::String> command_HeroExport(bs::Vector<bs::String> args);
+    void command_HeroExport(bs::Vector<bs::String> args);
 
     /**
      * Import attributes and abilities from the file with the given name in the save folder and apply
      * them to the currently controlled character.
      *
-     * @return A vector of output messages to be displayed by the ConsoleUI component.
      *
      * @param  args
      *           Name of the file.
      */
-    bs::Vector<bs::String> command_HeroImport(bs::Vector<bs::String> args);
+    void command_HeroImport(bs::Vector<bs::String> args);
 
     /**
      * Register a command by
@@ -301,7 +275,7 @@ namespace REGoth
   private:
     HUIConsole mConsoleUI;
     bs::Map<bs::String, Command> mCommands;
-    using commandCallback = bs::Vector<bs::String> (Console::*)(bs::Vector<bs::String>);
+    using commandCallback = void (Console::*)(bs::Vector<bs::String>);
 
     /**
      * A collection of all types of tokesn that can appear in a command like the command itself and
