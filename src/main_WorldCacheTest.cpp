b39d344706418d1eea768ff190e9751bb286d238
Andre Taulien <a.taulien@live.de>
2019-08-09 10:37:07 +0200

    Refactor: Simplify REGothWorldCacheTest

--- src/main_WorldCacheTest.cpp
+++ src/main_WorldCacheTest.cpp
@@ -2,64 +2,27 @@
 
 #include <BsFPSCamera.h>
 #include <Components/BsCCamera.h>
-#include <FileSystem/BsFileSystem.h>
-#include <GUI/BsCGUIWidget.h>
-#include <Resources/BsResources.h>
-#include <Scene/BsPrefab.h>
-#include <Scene/BsSceneObject.h>
 #include <Utility/BsTimer.h>
 
-#include <BsZenLib/ImportPath.hpp>
-#include <daedalus/DATFile.h>
-
 #include <core.hpp>
-#include <components/AnchoredTextLabels.hpp>
-#include <components/GameWorld.hpp>
-#include <components/Item.hpp>
-#include <components/Waynet.hpp>
-#include <components/Waypoint.hpp>
 #include <exception/Throw.hpp>
 #include <log/logging.hpp>
 #include <original-content/VirtualFileSystem.hpp>
+#include <BsZenLib/CacheUtility.hpp>
 
 class REGothWorldCacheTest : public REGoth::EmptyGame
 {
 public:
   using REGoth::EmptyGame::EmptyGame;
 
-  void setupMainCamera() override
-  {
-    REGoth::Engine::setupMainCamera();
-
-    mMainCamera->SO()->addComponent<bs::FPSCamera>();
-  }
-
   void setupScene() override
   {
     using namespace REGoth;
 
-    const bs::String world    = "OLDWORLD.ZEN";
-    const bs::String saveGame = "MySafeGame";
 
-    if (!bs::FileSystem::exists(BsZenLib::GothicPathToCachedWorld(saveGame)))
-    {
-      REGOTH_LOG(Info, Uncategorized, "[REGothWorldCacheTest] Importing and caching ZEN: {0}",
-                 world);
+    BsZenLib::CacheWholeVDFS(gVirtualFileSystem().getFileIndex());
 
-      HGameWorld gameWorld = GameWorld::importZEN(world);
-
-      REGOTH_LOG(Info, Uncategorized, "[REGothWorldCacheTest] Saving world...");
-
-      gameWorld->save(saveGame);
-    }
-    else
-    {
-      REGOTH_LOG(Info, Uncategorized, "[REGothWorldCacheTest] Loading world from SaveGame: {0}",
-                 world);
-
-      bs::HPrefab world = GameWorld::load(saveGame);
-      world->instantiate();
-    }
+    REGOTH_LOG(Info, Uncategorized, "Finished caching");
   }
 };
 
