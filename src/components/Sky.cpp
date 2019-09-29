3f255e9fc0ec45d2022f7191b74a45f6f05f2799
charlydelta <git@charlydelta.de>
2019-09-29 19:00:54 +0200

    Feature: Basic implementation to handle plane and dome differentiation

--- src/components/Sky.cpp
+++ src/components/Sky.cpp
@@ -1,65 +1,109 @@
 #include <components/Sky.hpp>
 
+#include <functional>
 #include <memory>
 
+#include <Components/BsCRenderable.h>
+#include <Material/BsMaterial.h>
 #include <RenderAPI/BsViewport.h>
 #include <Renderer/BsCamera.h>
+#include <Resources/BsBuiltinResources.h>
 #include <Scene/BsSceneManager.h>
+
+#include <BsZenLib/ImportMaterial.hpp>
+#include <BsZenLib/ImportTexture.hpp>
 
 #include <RTTI/RTTI_Sky.hpp>
 #include <components/GameClock.hpp>
 #include <components/GameWorld.hpp>
 #include <components/SkyStateGenerator.hpp>
+#include <exception/Assert.hpp>
 #include <exception/Throw.hpp>
+#include <original-content/VirtualFileSystem.hpp>
 
-namespace REGoth
+using namespace REGoth;
+
+Sky::Sky(const bs::HSceneObject& parent, HGameWorld gameWorld, const RenderMode& renderMode,
+         const bs::Color& skyColor)
+    : bs::Component{parent}
+    , mSkyStateGen{skyColor, gameWorld->worldName()}
+    , mGameWorld{gameWorld}
+    , mRenderMode{renderMode}
+    , mSkyRenderer{
+          std::bind(renderMode == RenderMode::Plane ? &Sky::renderSkyPlane : &Sky::renderSkyDome,
+                    this, std::placeholders::_1)}
 {
-  Sky::Sky(const bs::HSceneObject& parent, HGameWorld gameWorld, const RenderMode& renderMode,
-           const bs::Color& skyColor)
-      : bs::Component{parent}
-      , mSkyStateGen{skyColor, gameWorld->worldName()}
-      , mGameWorld{gameWorld}
-      , mRenderMode{renderMode}
-  {
-    setName("Sky");
+  setName("Sky");
+}
 
-    // TODO: Implement sky dome support for Gothic II.
-    if (renderMode == RenderMode::Dome)
+Sky::~Sky()
+{
+  // pass
+}
+
+void Sky::onInitialized()
+{
+  switch (mRenderMode)
+  {
+    case RenderMode::Plane:
     {
+      const bs::HShader shader =
+          bs::BuiltinResources::instance().getBuiltinShader(bs::BuiltinShader::Transparent);
+      mSkyMaterial = bs::Material::create(shader);
+
+      bs::HSceneObject so = bs::SceneObject::create("SkyPlane");
+      so->setPosition(bs::Vector3{0, 200, 0});
+      mSkyRenderable = so->addComponent<bs::CRenderable>();
+
+      const bs::HMesh mesh = bs::gBuiltinResources().getMesh(bs::BuiltinMesh::Quad);
+      mSkyRenderable->setMesh(mesh);
+      mSkyRenderable->setMaterial(mSkyMaterial);
+
+      const float scale = 20000.0f;
+      so->setScale(bs::Vector3{scale, 1.0f, scale});
+      break;
+    }
+
+    case RenderMode::Dome:
+    {
+      // TODO: Implement sky dome rendering.
       REGOTH_THROW(NotImplementedException, "Dome sky not yet implemented.");
+      break;
     }
   }
+}
 
-  Sky::~Sky()
-  {
-    // pass
-  }
+void Sky::update()
+{
+  // Update the sky state.
+  const float dayRatio = mGameWorld->gameclock()->getDayRatio();
+  REGOTH_ASSERT_VAL(dayRatio, 0.f <= dayRatio && dayRatio < 1.f, "Must be in [0, 1)");
+  std::shared_ptr<const SkyState> skyState = mSkyStateGen.update(dayRatio);
 
-  void Sky::update()
-  {
-    // Update the sky state.
-    const float dayRatio                     = mGameWorld->gameclock()->getDayRatio();
-    std::shared_ptr<const SkyState> skyState = mSkyStateGen.update(dayRatio);
+  // Render fog and sky.
+  renderFog(skyState->fogColor, skyState->fogNear, skyState->fogFar);
+  mSkyRenderer(skyState);
+}
 
-    // Render fog and sky.
-    renderFog(skyState->fogColor, skyState->fogNear, skyState->fogFar);
-    renderSky();
-  }
+void Sky::renderFog(const bs::Color& fogColor, float fogNear, float fogFar) const
+{
+  REGOTH_ASSERT(fogNear < fogFar, "fogNear must be smaller than fogFar");
 
-  void Sky::renderFog(const bs::Color& fogColor, float fogNear, float fogFar) const
-  {
-    // TODO: Account for distances.
-    (void)fogNear;
-    (void)fogFar;
+  // TODO: Account for distances using fogNear and fogFar.
 
-    const auto& camera = bs::gSceneManager().getMainCamera();
-    camera->getViewport()->setClearColorValue(fogColor);
-  }
+  const auto& camera = bs::gSceneManager().getMainCamera();
+  camera->getViewport()->setClearColorValue(fogColor);
+}
 
-  void Sky::renderSky() const
-  {
-    // TODO: Render sky.
-  }
+void Sky::renderSkyPlane(std::shared_ptr<const SkyState> skyState) const
+{
+  mSkyMaterial->setTexture("gAlbedoTex", skyState->skyLayer.texture);
+}
 
-  REGOTH_DEFINE_RTTI(Sky)
-}  // namespace REGoth
+void Sky::renderSkyDome(std::shared_ptr<const SkyState> /* skyState */) const
+{
+  // TODO: Implement sky dome rendering.
+  REGOTH_THROW(NotImplementedException, "Dome sky not yet implemented.");
+}
+
+REGOTH_DEFINE_RTTI(Sky)
--- src/components/Sky.hpp
+++ src/components/Sky.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <functional>
+#include <memory>
+
 #include <Image/BsColor.h>
 #include <Scene/BsComponent.h>
 
@@ -12,13 +15,16 @@ namespace REGoth
   using HGameWorld = bs::GameObjectHandle<GameWorld>;
 
   /**
-   * TODO: Documentation of Sky
+   * @brief The `Sky` component renders the sky, either as plane or dome.
+   *
+   * The render mode can be specified using the `renderMode` construction parameter.
    */
   class Sky : public bs::Component
   {
   public:
     /**
-     * The utilised render mode, i.e. render the sky as a textured dome or a plane.
+     * @brief The utilized render mode, i.e., whether to render the sky as a textured dome or a
+     *        plane.
      */
     enum class RenderMode
     {
@@ -26,19 +32,88 @@ namespace REGoth
       Plane
     };
 
+    /**
+     * @brief Constructs a `Sky` component.
+     * @param parent Parent scene object.
+     * @param gameWorld Instance to the `GameWorld` component.
+     * @param renderMode Render mode to be used, i.e., `RenderMode::Plane` or `RenderMode::Dome`.
+     * @param skyColor Base sky color.
+     */
     Sky(const bs::HSceneObject& parent, HGameWorld gameWorld, const RenderMode& renderMode,
         const bs::Color& skyColor);
+
+    /**
+     * @brief Destructs the `Sky` component.
+     */
     virtual ~Sky() override;
 
+    /**
+     * @see bs::Component::onInitialized
+     */
+    void onInitialized() override;
+
+    /**
+     * @brief bs::Component::update
+     */
     void update() override;
 
   private:
+    /**
+     * @brief Renders the fog.
+     * @param color Fog color.
+     * @param fogNear Near distance.
+     * @param fogFar Far distance.
+     */
     void renderFog(const bs::Color& color, float fogNear, float fogFar) const;
-    void renderSky() const;
 
+    /**
+     * @brief Renders the sky plane.
+     * @param skyState Current interpolated sky state.
+     */
+    void renderSkyPlane(std::shared_ptr<const SkyState> skyState) const;
+
+    /**
+     * @brief Renders the sky dome.
+     * @param skyState Current interpolated sky state.
+     *
+     * @note Not yet implemented, therefore tagged as [[noreturn]].
+     * @throw NotImplementedException.
+     */
+    [[noreturn]] void renderSkyDome(std::shared_ptr<const SkyState> skyState) const;
+
+    /**
+     * @brief Instance of a `SkyStateGenerator` used to interoplate key sky states.
+     */
     SkyStateGenerator mSkyStateGen{bs::Color{}, bs::String{}};
+
+    /**
+     * @brief Instance of the current `GameWorld`.
+     */
     HGameWorld mGameWorld;
+
+    /**
+     * @brief Currently used render mode.
+     */
     const RenderMode mRenderMode = RenderMode::Plane;
+
+    /**
+     * @brief Relevant sky rendering function (either `renderSkyPlane()` or `renderSkyDome()`), set
+     *        at construction depending on the `renderMode` construction parameter.
+     *
+     * @note Calling `mSkyRenderer` is cheaper than evaluating `mRenderMode` in each call of
+     *       `update()`.
+     */
+    const std::function<void(std::shared_ptr<const SkyState>)> mSkyRenderer;
+
+    /**
+     * @brief Used sky material.
+     */
+    bs::HMaterial mSkyMaterial;
+
+    /**
+     * @brief Used sky renderable.
+     */
+    bs::HRenderable mSkyRenderable;
 
   public:
     REGOTH_DECLARE_RTTI(Sky)
