966c7e2ec1c5c2be1d636a5cf0ae914e0fb94eae
Andre Taulien <a.taulien@live.de>
2019-11-02 21:54:21 +0100

    Build: Remove vcpkg from CI and docs

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -9,14 +9,9 @@ wget https://dilborceisv8p.cloudfront.net/bsf_2019.09.18_linux.tar.gz
 mkdir bsf
 tar -xf bsf_*.tar.gz -C bsf
 
-vcpkg update
-vcpkg install libsquish physfs --triplet x64-linux
-
 cmake -Dbsf_INSTALL_DIR=`pwd`/bsf \
       -DCMAKE_BUILD_TYPE=Release \
       -DREGOTH_USE_SYSTEM_BSF=On \
-      -DSKIP_AUTOMATE_VCPKG=On \
-      -DCMAKE_TOOLCHAIN_FILE="${VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake" \
       ../..
 
 cmake --build . --config Release --parallel
--- ci/build-windows.sh
+++ ci/build-windows.sh
@@ -7,14 +7,9 @@ cd build
 curl -O https://dilborceisv8p.cloudfront.net/bsf_2019.09.18_win64.zip
 unzip bsf_*.zip -d bsf
 
-vcpkg update
-vcpkg install libsquish physfs --triplet x64-windows
-
 cmake -Dbsf_INSTALL_DIR=`pwd`/bsf \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DREGOTH_USE_SYSTEM_BSF=On \
-      -DSKIP_AUTOMATE_VCPKG=On \
-      -DCMAKE_TOOLCHAIN_FILE="${VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake" \
       -A x64 \
       ../..
 
